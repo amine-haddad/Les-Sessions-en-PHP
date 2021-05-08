b447a715d12ea00508ce9346b5d8c5ab693de00c
amine-haddad <seniortouco@outlook.fr>
2021-05-08 22:56:19 +0200

    fin de la quette

---
+++ cart.php
@@ -0,0 +1,34 @@
+<?php require 'inc/head.php'; 
+    require 'inc/data/products.php';
+if(!isset($_SESSION['loginname'])){
+    header('Location: login.php');
+}
+?>
+<section class="cookies container-fluid">
+    <div class="row">
+        TODO : Display shopping cart items from $_SESSION here.
+        <?php 
+        var_dump($_SESSION);
+        ?>
+    </div>
+</section>
+<?php require 'inc/foot.php'; ?>
+
+
+ <div class="row">
+        <?php foreach ($catalog as $id => $cookie) { ?>
+            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3">
+                <figure class="thumbnail text-center">
+                    <img src="assets/img/product-<?= $id; ?>.jpg" alt="<?= $cookie['name']; ?>" class="img-responsive">
+                    <figcaption class="caption">
+                        <h3><?= $cookie['name']; ?></h3>
+                        <p><?= $cookie['description']; ?></p>
+                        <a href="?add_to_cart=<?= $id; ?>" class="btn btn-primary">
+                            <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Add to cart
+                        </a>
+                    </figcaption>
+                </figure>
+            </div>
+
+        <?php } ?>
+    </div>
---
+++ login.php
@@ -0,0 +1,80 @@
+
+<?php require 'inc/head.php'; ?>
+<?php
+if (isset($_SESSION['loginname'])){
+    header('location: /');
+}
+    
+    if ($_SERVER['REQUEST_METHOD']=== 'POST'){
+        // securisé les données
+        $errors=[];
+        if (!empty ($_POST['loginname'])){
+            $_SESSION['loginname'] = $_POST['loginname'];
+            header('Location: index.php');
+        }else{
+
+           $errors['loginname']='indiquez un nom';
+        }
+    }
+     if (!empty ($_SESSION['loginname']))
+     {
+        // Echo session variables that were set on previous page
+        echo "<strong>Hello  " . $_SESSION["loginname"] . " ! </strong>.<br>";
+
+    }else{
+       echo '<strong>Hello Wilder ! </strong>';
+
+    }
+    ?>
+<div class="container" style="margin-top:40px">
+    <div class="row">
+        <div class="col-sm-6 col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <strong> Sign in to continue</strong>
+                </div>
+                <div class="panel-body">
+                    <form role="form" action="#" method="POST">
+                        <fieldset>
+                            <div class="row">
+                                <div class="center-block">
+                                    <img class="profile-img"
+                                         src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
+                                         alt="">
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-sm-12 col-md-10  col-md-offset-1 ">
+                                    <div class="form-group">
+                                        <div class="input-group">
+                                            <span class="input-group-addon">
+                                              <i class="glyphicon glyphicon-user"></i>
+                                            </span>
+                                            <input class="form-control" placeholder="Username" name="loginname"
+                                                   type="text" autofocus>
+                                        </div>
+                                        <div>
+                                            <?php
+                                            if (!empty($errors)){
+                                                echo $errors['loginname'];
+                                            }
+                                            ?>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="submit" class="btn btn-lg btn-primary btn-block" value="Sign in">
+                                    </div>
+                                </div>
+                            </div>
+                        </fieldset>
+                    </form>
+                </div>
+                <?php var_dump($_SESSION); ?>
+                <div class="panel-footer ">
+                    Don't have an account ? <a href="#" onClick="">Too bad !</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php require 'inc/foot.php'; ?>
